9e0ea9cc3b435b3b834bfbdace9b54b20cd16699
vlxcoding <vlxcoding@gmail.com>
2024-03-05 18:59:48 -0500

    Exercise 3-4 (Completed) -Added a user to database so you can login & Updated the /login route.

--- app/views.py
+++ app/views.py
@@ -5,6 +5,7 @@ from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.utils import secure_filename
 from app.models import UserProfile
 from app.forms import LoginForm
+from werkzeug.security import check_password_hash
 
 
 ###
@@ -43,20 +44,25 @@ def login():
 
     # change this to actually validate the entire form submission
     # and not just one field
-    if form.username.data:
+    if form.validate_on_submit():
         # Get the username and password values from the form.
-
+        username = form.username.data
+        password = form.password.data
+        user = db.session.execute(db.select(UserProfile).filter_by(username=username)).scalar()
+        if user and check_password_hash(user.password, password):
+            # Remember to flash a message to the user
+            flash('Login successful!', 'success')
+            # Gets user id, load into session
+            login_user(user)
+            # The user should be redirected to the upload form instead
+            return redirect(url_for('upload'))
+        else:
+            flash('Invalid username or password. Please try again.', 'danger')
         # Using your model, query database for a user based on the username
         # and password submitted. Remember you need to compare the password hash.
         # You will need to import the appropriate function to do so.
         # Then store the result of that query to a `user` variable so it can be
         # passed to the login_user() method below.
-
-        # Gets user id, load into session
-        login_user(user)
-
-        # Remember to flash a message to the user
-        return redirect(url_for("home"))  # The user should be redirected to the upload form instead
     return render_template("login.html", form=form)
 
 # user_loader callback. This callback is used to reload the user object from
